a87a44c10aeb66ef9ee30281c04990e951655036
ariksidney <ariksidney@outlook.com>
2019-04-12 17:02:50 +0200

    Save values in new DB structure. Values are saved in "values" object

--- src/main/java/ch/iceguard/functions/Function.java
+++ src/main/java/ch/iceguard/functions/Function.java
@@ -23,6 +23,7 @@ public class Function {
     private static final String MESSAGE_ID = "messageId";
     private static final String DEVICE_ID = "deviceId";
     private static final String TIMESTAMP = "timestamp";
+    private static final String MEASUREMENT_VALUE_FIELD = "values";
 
     @FunctionName("iot-cosmos-processor")
     @StorageAccount("storageAccount")
@@ -31,17 +32,23 @@ public class Function {
             final ExecutionContext context) throws IOException {
 
         JsonFactory factory = new JsonFactory();
-        JsonParser parser = factory.createParser(message);
-        Document document = new Document();
+        Document document;
+        Document measurementValues;
+        System.out.println(message);
+        try (JsonParser parser = factory.createParser(message)) {
+            document = new Document();
+            measurementValues = new Document();
 
-        while (!parser.isClosed()) {
-            JsonToken jsonToken = parser.nextToken();
+            while (!parser.isClosed()) {
+                JsonToken jsonToken = parser.nextToken();
 
-            if (JsonToken.FIELD_NAME.equals(jsonToken)) {
-                parser.nextToken();
-                setDocumentValue(parser, document);
+                if (JsonToken.FIELD_NAME.equals(jsonToken)) {
+                    parser.nextToken();
+                    setDocumentValue(parser, document, measurementValues);
+                }
             }
         }
+        document.append(MEASUREMENT_VALUE_FIELD, measurementValues);
         validateDocument(document);
 
         try (MongoClient mongoClient = new MongoClient(new MongoClientURI(System.getenv("iceguard-iot_DOCUMENTDB")))) {
@@ -68,7 +75,7 @@ public class Function {
         return database.getCollection(System.getenv("collection-name-COSMOS"));
     }
 
-    private static void setDocumentValue(JsonParser parser, Document document) throws IOException {
+    private static void setDocumentValue(JsonParser parser, Document document, Document measurementValues) throws IOException {
         String fieldName = parser.getCurrentName();
         if (MESSAGE_ID.equals(fieldName)) {
             document.append(MESSAGE_ID, parser.getValueAsInt());
@@ -77,31 +84,31 @@ public class Function {
             document.append(DEVICE_ID, parser.getValueAsString());
         }
         if ("temperature".equals(fieldName)) {
-            document.append("temperature", parser.getValueAsDouble());
+            measurementValues.append("temperature", parser.getValueAsDouble());
         }
         if ("humidity".equals(fieldName)) {
-            document.append("humidity", parser.getValueAsDouble());
+            measurementValues.append("humidity", parser.getValueAsDouble());
         }
         if (TIMESTAMP.equals(fieldName)) {
             document.append(TIMESTAMP, parser.getValueAsDouble());
         }
         if ("acceleratorX".equals(fieldName)) {
-            document.append("acceleratorX", parser.getValueAsDouble());
+            measurementValues.append("acceleratorX", parser.getValueAsDouble());
         }
         if ("acceleratorY".equals(fieldName)) {
-            document.append("acceleratorY", parser.getValueAsDouble());
+            measurementValues.append("acceleratorY", parser.getValueAsDouble());
         }
         if ("acceleratorZ".equals(fieldName)) {
-            document.append("acceleratorZ", parser.getValueAsDouble());
+            measurementValues.append("acceleratorZ", parser.getValueAsDouble());
         }
         if ("gyroscopeX".equals(fieldName)) {
-            document.append("gyroscopeX", parser.getValueAsDouble());
+            measurementValues.append("gyroscopeX", parser.getValueAsDouble());
         }
         if ("gyroscopeY".equals(fieldName)) {
-            document.append("gyroscopeY", parser.getValueAsDouble());
+            measurementValues.append("gyroscopeY", parser.getValueAsDouble());
         }
         if ("gyroscopeZ".equals(fieldName)) {
-            document.append("gyroscopeZ", parser.getValueAsDouble());
+            measurementValues.append("gyroscopeZ", parser.getValueAsDouble());
         }
     }
 }
